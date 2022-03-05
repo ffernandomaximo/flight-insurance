ca4b1301e71bca1c4a1cfff8306d1ac96d4a3f30
Fernando Maximo Ferreira <fernandomaximof@Fernandos-MBP.hub>
2022-03-05 13:56:22 +1300

    added interface

--- truffle-config.js
+++ truffle-config.js
@@ -1,33 +1,53 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const infuraKey = "ce1b404ad7334078bf3d10382c6966ee";
-
-const mnemonic = ""
-
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+require('dotenv').config()
+ 
+const mnemonic = process.env.MNEMONIC
+const url = process.env.KOVAN_RPC_URL
+const rinkeby_url = process.env.RINKEBY_RPC_URL
+ 
 module.exports = {
   networks: {
-    development: {
-      host: "127.0.0.1",
+    cldev: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+    },
+    ganache: {
+      host: '127.0.0.1',
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: '*',
+    },
+    binance_testnet: {
+      provider: () => new HDWalletProvider(mnemonic,'https://data-seed-prebsc-1-s1.binance.org:8545'),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    kovan: {
+      provider: () => {
+        return new HDWalletProvider(mnemonic, url)
+      },
+      network_id: '42',
+      skipDryRun: true
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
-        network_id: 4,       // rinkeby's id
-        gas: 10000000,        // rinkeby has a lower block limit than mainnet
-        gasPrice: 10000000000
-    }
+      provider: () => {
+        return new HDWalletProvider(mnemonic, rinkeby_url)
+      },
+      network_id: '4',
+      skipDryRun: true
+    },
   },
   compilers: {
     solc: {
-      version: "^0.8.0"
-    }
+      version: '^0.8.0',
+    },
   },
-  solc: {
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1500
-      }
-    }
-  }
-};
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ]
+}
